08e0146888faf70c0d54197cde05a76fe29319c9
xingwenpeng84355 <843553050@qq.com>
2019-12-23 00:55:20 +0000

    Initial Commit

---
+++ DataStructureGo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DataStructureGo
+//
+//  Created by xingwenpeng on 23/12/2019.
+//  Copyright © 2019 xingwenpeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
